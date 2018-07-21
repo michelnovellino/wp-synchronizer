7b2802c29158f2295ea58a6e91db8282d3cc567f
michelnovellino <michelnovellino.programador@gmail.com>
2018-07-21 19:08:51 -0400

    agregando funciones adicionales de configuracion

---
+++ includes/synchronizer_functions.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+if(!function_exists("synchronizer_getMovies")){
+
+function synchronizer_getMovies($atts) {
+    global $wpdb;
+    $args = shortcode_atts(
+        array(
+            'baseUrl'=> "https://jsonplaceholder.typicode.com/posts/",
+            'url' => '1'
+        ),$atts
+    );
+    
+    $baseUrl = $args['baseUrl'];
+    $Url = $args["url"];
+
+        $headers = array('Accept' => 'application/json');
+        $response = Unirest\Request::get($baseUrl.$Url , $headers);
+
+        echo "<br>".$response->body->userId;
+        echo "<br>".$response->body->title;
+        echo "<br>".$response->body->body."<br><br>";
+        $myrows = $wpdb->get_results( "SELECT id FROM wp_users" );            
+        echo $myrows[0]->id;
+        
+    
+ }
+}
+add_shortcode('api-example', 'synchronizer_getMovies'); 
+
+?>
--- index.php
+++ index.php
@@ -1,37 +1,110 @@
 <?php 
 
+
+
 /*
+
 Plugin Name: wp - synchronizer 
+
 Plugin Uri: url
+
 Description : sincronizador de wordpress
+
 Author: Michel Novellino
+
 Version: 0.9
+
 Author Uri: github.com/michelnovellino
 
 
+
+
+
 */
+
 require_once __DIR__.'/unirest/src/Unirest.php';
 
-function get_movies($atts) {
+require_once  __DIR__.'/includes/synchronizer_functions.php';
 
-    $args = shortcode_atts(
-        array(
-            'baseUrl'=> "https://jsonplaceholder.typicode.com/posts/",
-            'url' => '1'
-        ),$atts
-    );
-    
 
-		$headers = array('Accept' => 'application/json');
-		$response = Unirest\Request::get($baseUrl.$arg["url"] , $headers);
 
-		echo "<br>".$response->body->userId;
-		echo "<br>".$response->body->title;
-		echo "<br>".$response->body->body;
-	
 
-	
- }
- add_shortcode('api-example', 'get_movies'); 
 
-?>
+add_action( 'admin_menu', 'wp_sync_options' );
+
+
+
+    if(!function_exists("wp_sync_options")){
+
+    function wp_sync_options(){
+
+        add_menu_page(
+
+            'Configuracion de sincronizacion', // Title of the page
+
+            'Sincronizar', // Text to show on the menu link
+
+            'manage_options', // Capability requirement to see the link
+
+            'custom-page',// The 'slug' - file to display when clicking the link
+
+            'custom',
+
+            'dashicons-megaphone'
+
+            );
+
+        
+
+    }
+
+    }
+
+    ?>
+
+<?php
+
+    if(!function_exists("custom")){
+
+        
+
+        function custom(){
+
+            ?>
+
+            <div class="wrap">
+
+                <h1>bienvenido</h1>
+
+                <p>asistente de sincronizacion`</p>
+
+                <button style="
+
+                padding: 0.7%;
+
+                background: #00a0d2;
+
+                color: white;
+
+                box-shadow: 1px 1px #555d66;
+
+                border-color: #00a0d2;
+
+                border-radius: 13px;"
+
+                 >sincronizar</button>
+
+            </div>
+
+    <?php
+
+        }
+
+    }
+
+
+
+   
+
+   
+
